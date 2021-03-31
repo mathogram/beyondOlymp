1bd87e3a426238268711140fe02332a816808e39
Mansur <funisinfinity26@gmail.com>
2021-03-31 06:19:05 +0000

    bmo

--- main.py
+++ main.py
@@ -16,14 +16,14 @@ def clean(str):
 
 
 for olymp in y:
-	if olymp["title"].find("Олимпиада Туймаада по математике. Младшая лига.")+1:
+	if olymp["title"].find("Балканская математическая олимпиада")+1:
 		print(olymp["title"])
 		year = olymp["title"].split(" ")[-2]
 		# group = olymp["title"].split(" ")[7]
-		group = "junior"
+		# group = "junior"
 
 		for problem in range(len(olymp["problems"])):
-			file_path = "tuimada/"+ str(year) +"/" + str(group)
+			file_path = "bmo/"+ str(year) #+"/" + str(group)
 
 			if not os.path.exists(file_path):
 				os.makedirs(file_path)
